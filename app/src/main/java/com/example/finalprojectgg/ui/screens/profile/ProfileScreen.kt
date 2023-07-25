e3dfe47f54692ca1fd4791a7c9aedf0fd553e1ad
ddenfi <deki.nfi@gmail.com>
2023-07-26 00:22:15 +0700

    feat-fix:Profile Screen
    UI
    +Add componentes

--- app/src/main/java/com/example/finalprojectgg/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/finalprojectgg/ui/screens/profile/ProfileScreen.kt
@@ -5,58 +5,125 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowRight
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen() {
-    BottomSheetScaffold(sheetContent = {
-        Column(
-            Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.surface)
-        ) {
-
-        }
-    }) {
+    Column(Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
-                .padding(bottom = it.calculateBottomPadding())
                 .fillMaxWidth()
-        )
-    }
-}
+                .fillMaxHeight(0.5f)
+                .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f))
+        ) {
+            AsyncImage(
+                model = "https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
 
-@Composable
-fun DropDownItem(test: Sport) {
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .wrapContentSize()
-    ) {
-        Text(text = test.emoji)
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(test.name)
-    }
-}
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AsyncImage(
+                    model = "https://images.unsplash.com/photo-1504593811423-6dd665756598?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    text = "Mubariz Mehdizadeh",
+                    style = MaterialTheme.typography.headlineSmall.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                )
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.surface
+                )
+            }
+        }
+        
+        Column(Modifier.padding(horizontal = 24.dp)) {
+            Card(onClick = { /*TODO*/ } ) {
+                Column(modifier = Modifier.padding(all = 16.dp)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        ) {
+                            Icon(imageVector = Icons.Default.Place, contentDescription = null)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                text = "Set Default Location",
+                                style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                            )
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.ArrowRight, contentDescription = null)
+                        }
+                    }
+                    Spacer(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(MaterialTheme.colorScheme.outline)
+                        .padding(vertical = 2.dp))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .weight(1f)) {
+                            Icon(imageVector = Icons.Default.DarkMode, contentDescription = null)
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Set Theme", style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold))
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.ArrowRight, contentDescription = null)
+                        }
+                    }
+                }
 
-data class Sport(
-    val name: String,
-    val emoji: String
-) {
-    override fun toString(): String {
-        return "$emoji $name"
+            }
+        }
+        
     }
 }
